17206b1c617c1a4d2bf943c8da86f35f51eca8c9
laiscrz <lais.albino2003@gmail.com>
2023-10-29 14:55:49 -0300

    Add melhorias em cabeçalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -4,29 +4,28 @@ import Link from "next/link";
 export default function Cabecalho() {
 
     return (
-        <div className='cabecalho-conteiner'>
-            <header>
-                <Image src={'/Image/logoinovax_bik.png'} alt="Logo Inovax" width={'120'} height={'120'} className="logo" />
-                <nav>
-                    <div className='navegacao-links'>
-                        <Link href={'/'}>Home</Link>
-                        <Link href={'/ComoFunciona'}>Como Funciona</Link>
-                        <Link href={'/Servicos'}>Serviços</Link>
-                        <Link href={'/SobreNos'}>Sobre Nós</Link>
-                        <Link href={'/Contato'}>Contato</Link>
+        <header className='cabecalho-conteiner'>
+            <Image src={'/Image/logoinovax_bik.png'} alt="Logo Inovax" width={'120'} height={'120'} className="logo" />
+            <nav>
+                <div className='navegacao-links'>
+                    <Link href={'/'}>Home</Link>
+                    <Link href={'/ComoFunciona'}>Como Funciona</Link>
+                    <Link href={'/Servicos'}>Serviços</Link>
+                    <Link href={'/SobreNos'}>Sobre Nós</Link>
+                    <Link href={'/Contato'}>Contato</Link>
 
-                    </div>
-                    <div className="acessar-login">
-                        <Link href={'/Login'}>
-                            <button id="btnLogin">
-                                <span><Image src={'/Image/profile_icon.png'} alt="Profile Icon" width={'30'} height={'30'} />Login</span>
-                            </button>
-                        </Link>
-                    </div>
-                </nav>
+                </div>
+                <div className="acessar-login">
+                    <Link href={'/Login'}>
+                        <button id="btnLogin">
+                            <span><Image src={'/Image/profile_icon.png'} alt="Profile Icon" width={'30'} height={'30'} />Login</span>
+                        </button>
+                    </Link>
+                </div>
+            </nav>
 
-            </header>
-        </div>
+        </header>
+
 
 
     )
