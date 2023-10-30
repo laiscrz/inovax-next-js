c76e8f8056c13b7053a18655c2212f87ee3acc14
lucasdslopes <lucas.dslopes12@gmail.com>
2023-10-30 17:05:38 -0300

    Add estilização em Integrantes + Conteudo

--- src/app/Integrantes/page.jsx
+++ src/app/Integrantes/page.jsx
@@ -1,7 +1,21 @@
-export default function Integrantes(){
+import 'src/app/Integrantes/integrantes.css';
+
+export default function Integrantes() {
     return (
         <main>
-            <h1>Integrantes</h1>
+            <div className="integrantes-conteiner">
+                <section class="integrantes">
+                    <h1>Integrantes</h1>
+                    <p> Gustavo Marques Catelan - RM 551823 </p>
+                    <p> Lais Alves da Silva Cruz - RM 552258</p>
+                    <p> Lucas dos Santos Lopes - RM 550790 </p>
+                    <p> Murilo Machado - RM 550718</p>
+                    <p> Victor Taborda - RM 97900</p>
+
+                    <h1>Link do Repositório Git/GitHub</h1>
+                    <p>Decidimos compartilhar o link do nosso repositório como uma referência. Você pode acessar o repositório <a href="https://github.com/laiscrz/inovax-next-js">clicando aqui</a>.</p>
+                </section>
+            </div>
         </main>
     )
 }
