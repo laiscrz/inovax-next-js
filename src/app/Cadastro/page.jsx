7e7153489bf4a81553ab980b8faf82f9be917fa5
MuriloMach <murilomachado1210@gmail.com>
2023-10-31 17:22:54 -0300

    Add Estilização em Cadastro e seu conteudo

--- src/app/Cadastro/page.jsx
+++ src/app/Cadastro/page.jsx
@@ -1,7 +1,141 @@
-export default function Cadastro(){
+"use client"
+import Next, { useState } from 'react';
+import '/src/app/Cadastro/cadastro.css';
+export default function Cadastro() {
+    const [clientes, setClientes] = useState([]);
+    const [cliente, setCliente] = useState({
+        nome: '',
+        email: '',
+        telefone: '',
+        endereco: '',
+        cpf: '',
+        dataNascimento: '',
+        senha: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setCliente({ ...cliente, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const dataNascimento = new Date(cliente.dataNascimento);
+        const hoje = new Date();
+        const idade = hoje.getFullYear() - dataNascimento.getFullYear();
+
+        if (idade < 18) {
+            alert('Você não pode se cadastrar, pois não tem idade suficiente.');
+            return;
+        }
+
+        setClientes([...clientes, cliente]);
+
+        setCliente({
+            nome: '',
+            email: '',
+            telefone: '',
+            endereco: '',
+            cpf: '',
+            dataNascimento: '',
+            senha: '',
+        });
+
+        // Redirecionar apenas se os dados estiverem corretos
+        window.location.href = "/PortalCliente";
+    };
+
     return (
-        <main>
-            <h1>Cadastro</h1>
+        <main className="cadastro">
+            <div className="cadastro-conteiner">
+                <h1>Cadastro de Cliente</h1>
+                <p>Preencha os Dados Abaixo Corretamente.</p><br />
+                <div className="form-cadastro">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="nome">Nome Completo:</label><br />
+                            <input
+                                type="text"
+                                id="nome"
+                                name="nome"
+                                value={cliente.nome}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email:</label><br />
+                            <input
+                                type="email"
+                                id="email"
+                                name="email"
+                                value={cliente.email}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="telefone">Telefone:</label><br />
+                            <input
+                                type="tel"
+                                id="telefone"
+                                name="telefone"
+                                value={cliente.telefone}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="endereco">Endereço:</label><br />
+                            <input
+                                type="text"
+                                id="endereco"
+                                name="endereco"
+                                value={cliente.endereco}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="cpf">CPF:</label><br />
+                            <input
+                                type="text"
+                                id="cpf"
+                                name="cpf"
+                                value={cliente.cpf}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="dataNascimento">Data de Nascimento:</label><br />
+                            <input
+                                type="date"
+                                id="dataNascimento"
+                                name="dataNascimento"
+                                value={cliente.dataNascimento}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="senha">Senha:</label><br />
+                            <input
+                                type="password"
+                                id="senha"
+                                name="senha"
+                                value={cliente.senha}
+                                onChange={handleInputChange}
+                                required
+                            />
+                        </div><br />
+                        <span>
+                            <button type="submit">Cadastrar</button>
+                        </span>
+                    </form>
+                </div>
+            </div>
         </main>
-    )
+    );
 }
