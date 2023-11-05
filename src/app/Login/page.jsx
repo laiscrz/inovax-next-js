298f5b2570ec407c055eac78824276beaeb24ed5
laiscrz <lais.albino2003@gmail.com>
2023-11-04 21:46:31 -0300

    add page login + css

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -1,7 +1,75 @@
-export default function Login(){
+"use client"
+import '/src/app/Login/login.css'
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Email: ${email}, Senha: ${senha}`);
+        setEmail('');
+        setSenha('');
+    }
+
     return (
         <main>
-            <h1>Login</h1>
+            <div className="container">
+        <div className="container-login">
+          <div className="wrap-login">
+            <form className="login-form" onSubmit={handleSubmit}>
+              <span className="login-form-title">
+                Faça o seu login
+              </span><br />
+
+              <div className="wrap-input margin-bottom-35">
+                <input
+                  className="input-form"
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+                <span className="focus-input-form" data-placeholder="E-mail"></span>
+              </div>
+
+              <div className="wrap-input margin-bottom-35">
+                <input
+                  className="input-form"
+                  type="password"
+                  value={senha}
+                  onChange={(e) => setSenha(e.target.value)}
+                  required
+                />
+                <span className="focus-input-form" data-placeholder="Senha"></span>
+              </div>
+
+              <div className="container-login-form-btn">
+                <Link href={'/PortalCliente'}>
+                  <span><button className="login-form-btn" type="submit">
+                    Entrar
+                  </button></span>
+                </Link>
+              </div>
+
+              <ul className="login-utils">
+                <li className="margin-bottom-8 margin-top-8">
+                  <span className="text1">
+                    Não tem uma conta?
+                  </span>
+
+                  <Link href={'/Cadastro'} className="text2">
+                     Cadastre-se
+                  </Link>
+                </li>
+              </ul>
+            </form>
+          </div>
+          <img src="/Image/login.png" width="300" height="300" class="margin-left-50" />
+        </div>
+      </div>
         </main>
     )
 }
