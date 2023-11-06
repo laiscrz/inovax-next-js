51e34c31b9f8ebd06402e5885e64701353a2a821
laiscrz <lais.albino2003@gmail.com>
2023-11-06 18:08:08 -0300

    Add conteudo em "page.js" e estilização

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,42 @@
+import Link from 'next/link'
 import '/src/app/pagehome.css'
+import Carrossel from '@/components/Carrossel'
 
 export default function Home() {
+  const images = [
+    '/Image/feedback_clienteana.png',
+    '/Image/feedback_clientenoah.png',
+    '/Image/feedback_clienteestela.png',
+  ];
   return (
     <main>
-      <h1>Home</h1>
+      <div className="home-conteiner">
+        <section className='inicio-home'>
+          <div className="destaq-inicio">
+            <div className="frase-destaq">
+              <h1>Sua Vistoria</h1>
+              <h2>Online Aqui!</h2>
+            </div>
+            <div className="btn-saibamais">
+              <Link href={'/ComoFunciona'}>
+                <span><button>Saiba Mais</button></span>
+              </Link>
+            </div>
+          </div>
+          <div className="destaq-bicle">
+            <div className="quadrad-home">
+
+            </div>
+            <img src="/Image/img_homedestaq.png" alt="Homem na Bicicleta" />
+          </div>
+        </section>
+
+        <section className="feedbackclie-destaq">
+          <h2>FeedBack de Destaques</h2><br />
+          <p>Aqui estão alguns dos comentários e feedbacks de nossos clientes satisfeitos:</p><br />
+          <Carrossel imagens={images} />
+        </section>
+      </div>
     </main>
   )
 }
