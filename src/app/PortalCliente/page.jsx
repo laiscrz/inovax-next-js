9a9a9bc636c5b24131776e9b0457a68201f53de7
laiscrz <lais.albino2003@gmail.com>
2023-11-07 21:43:44 -0300

    add menu portal cliente e icons

--- src/app/PortalCliente/page.jsx
+++ src/app/PortalCliente/page.jsx
@@ -1,7 +1,76 @@
-export default function PortalCliente(){
+import React from 'react';
+import '/src/app/PortalCliente/portalcliente.css';
+import Link from 'next/link';
+
+const MenuOption = ({ texto, imagemSrc, caminho }) => (
+
+    <div className="menu-option">
+        <Link href={caminho}>
+            <img src={imagemSrc} alt={texto} className="menu-image" />
+            <div className="menu-text">{texto}</div>
+        </Link>
+    </div>
+);
+
+export default function PortalCliente() {
+    const menuOptions = [
+        {
+            texto: "Solicitar Nova Vistoria",
+            imagemSrc: "/Image/icon_solic.png",
+            caminho: '/PortalCliente/solicitar'
+        },
+        {
+            texto: "Acompanhar Vistorias em Andamento",
+            imagemSrc: "/Image/icon_acompa.png",
+            caminho: '/PortalCliente/solicitar'
+        },
+        {
+            texto: "Ver Histórico de Vistorias de Bicicletas",
+            imagemSrc: "/image/icon_hist.png",
+            caminho: '/PortalCliente/solicitar'
+        },
+        {
+            texto: "Configurações da Conta",
+            imagemSrc: "/image/icon_config.png",
+            caminho: '/PortalCliente/solicitar'
+        },
+        {
+            texto: "Suporte ao Cliente",
+            imagemSrc: "/image/icon_suporte.jpg",
+            caminho: '/PortalCliente/solicitar'
+        },
+        {
+            texto: "FAQ (Perguntas Frequentes)",
+            imagemSrc: "/image/icon_faq.jpg",
+            caminho: '/PortalCliente/solicitar'
+        },
+        {
+            texto: "Feedback",
+            imagemSrc: "/image/icon_feedback.jpg",
+            caminho: '/PortalCliente/solicitar'
+        },
+        {
+            texto: "Sair da Conta",
+            imagemSrc: "image/icon_sair.jpg",
+            caminho: '/PortalCliente/solicitar'
+        },
+
+    ];
+
     return (
         <main>
-            <h1>Portal Cliente</h1>
+            <div className="portalclie-conteiner">
+                <div className="menu-options">
+                    {menuOptions.map((option, index) => (
+                        <MenuOption
+                            key={index}
+                            texto={option.texto}
+                            imagemSrc={option.imagemSrc}
+                            caminho={option.caminho} // Certifique-se de incluir o caminho aqui
+                        />
+                    ))}
+                </div>
+            </div>
         </main>
-    )
+    );
 }
