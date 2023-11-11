8259a7a703b777396e66315ac000e26b80c5cbc6
laiscrz <lais.albino2003@gmail.com>
2023-11-11 18:24:16 -0300

    Add consumo de api CEP

--- src/app/Cadastro/page.jsx
+++ src/app/Cadastro/page.jsx
@@ -1,56 +1,94 @@
 "use client"
-import Next, { useState } from 'react';
 import '/src/app/Cadastro/cadastro.css';
 
-export default function Cadastro() {
+import React, { useState } from 'react';
 
-        const [clientes, setClientes] = useState([]);
-        const [cliente, setCliente] = useState({
-            nome: '',
-            cpf: '',
-            endereco: '',
-            telefone: '',
-            sexo: '',
-            email: '',
-            anoNasc: '',
-            senha: '',
-        });
-    
-        const handleInputChange = (e) => {
-            const { name, value } = e.target;
-            const parsedValue = name === 'anoNasc' ? parseInt(value, 10) : value;
-    
+export default function Cadastro() {
+    const [clientes, setClientes] = useState([]);
+    const [cliente, setCliente] = useState({
+        nome: '',
+        cpf: '',
+        endereco: '',
+        telefone: '',
+        sexo: '',
+        email: '',
+        anoNasc: '',
+        senha: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        const parsedValue = name === 'anoNasc' ? parseInt(value, 10) : value;
+
+        
+        if (name.startsWith('endereco.')) {
+            const enderecoProperty = name.split('.')[1]; 
+            setCliente({
+                ...cliente,
+                endereco: {
+                    ...cliente.endereco,
+                    [enderecoProperty]: parsedValue,
+                },
+            });
+        } else {
             setCliente({ ...cliente, [name]: parsedValue });
+        }
+    };
+
+    const formatEndereco = () => {
+        const { endereco } = cliente;
+        return endereco; 
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const clienteComEnderecoFormatado = {
+            ...cliente,
+            endereco: formatEndereco(),
         };
-    
-        const handleSubmit = async (e) => {
-            e.preventDefault();
-        
-            setClientes([...clientes, cliente]);
-        
-            try {
-                const response = await fetch("http://localhost:8080/07-WebApi/api/cliente", {
-                    method: "POST",
-                    headers: { "Content-Type": "application/json" },
-                    body: JSON.stringify(cliente),
-                });
-        
-                if (response.ok) {
-                    console.log("Cliente adicionado com sucesso!");
-                } else {
-                    console.error("Falha ao adicionar cliente.");
-                }
-            } catch (error) {
-                console.error("Erro na resposta do servidor:", error);
+
+        setClientes([...clientes, clienteComEnderecoFormatado]);
+
+        try {
+            const response = await fetch("http://localhost:5000/cliente", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(clienteComEnderecoFormatado),
+            });
+
+            if (response.ok) {
+                console.log("Cliente adicionado com sucesso!");
+                window.location.href = '/PortalCliente';
+            } else {
+                console.error("Falha ao adicionar cliente.");
             }
-        };
-        
-        
+        } catch (error) {
+            console.error("Erro na resposta do servidor:", error);
+        }
+    };
+
+    const buscarCep = (e) => {
+        const cep = e.target.value.replace(/\D/g, '');
+
+        fetch(`https://viacep.com.br/ws/${cep}/json/`)
+            .then((res) => res.json())
+            .then((data) => {
+                setCliente({
+                    ...cliente,
+                    endereco: `${data.logradouro || ''}, ${data.bairro || ''}, ${data.localidade || ''}, ${data.uf || ''}, ${cep}`,
+                });
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar CEP:", error);
+            });
+    };
+
     return (
         <main className="cadastro">
             <div className="cadastro-conteiner">
                 <h1>Cadastro de Cliente</h1>
-                <p>Preencha os Dados Abaixo Corretamente.</p><br />
+                <p>Preencha os Dados Abaixo Corretamente.</p>
                 <div className="form-cadastro">
                     <form onSubmit={handleSubmit}>
                         <div className="form-group">
@@ -82,17 +120,6 @@ export default function Cadastro() {
                                 id="telefone"
                                 name="telefone"
                                 value={cliente.telefone}
-                                onChange={handleInputChange}
-                                required
-                            />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="endereco">Endereço:</label><br />
-                            <input
-                                type="text"
-                                id="endereco"
-                                name="endereco"
-                                value={cliente.endereco}
                                 onChange={handleInputChange}
                                 required
                             />
@@ -131,6 +158,27 @@ export default function Cadastro() {
                             />
                         </div>
                         <div className="form-group">
+                            <label htmlFor="endereco">Endereço:</label><br />
+                            <input
+                                type="text"
+                                id="cep"
+                                placeholder="CEP"
+                                onChange={buscarCep}
+                                required
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label>Endereço Completo:</label><br />
+                            <input
+                                type="text"
+                                id="endereco"
+                                name="endereco"
+                                value={formatEndereco()}
+                                readOnly
+                            />
+
+                        </div>
+                        <div className="form-group">
                             <label htmlFor="senha">Senha:</label><br />
                             <input
                                 type="password"
@@ -140,9 +188,10 @@ export default function Cadastro() {
                                 onChange={handleInputChange}
                                 required
                             />
-                        </div><br />
+                        </div>
+                        <br />
                         <span>
-                            <button type="submit" >Cadastrar</button>
+                            <button type="submit">Cadastrar</button>
                         </span>
                     </form>
                 </div>
